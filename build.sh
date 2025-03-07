1a6ae20418f9ff49a716ad79c8ec6217562b2e03
tanguy maze <tanguy.maze@clever-cloud.com>
2025-03-07 17:50:09 +0100

    add custom build phase

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir /immich
+mkdir /immich-src
+
+./pre-install.sh
+./install.sh
+./post-install.sh
--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-mkdir /immich
-mkdir /immich-src
-
-./pre-install.sh
-./install.sh
-./post-install.sh
-
 /immich/app/machine-learning/start.sh &
 /immich/app/start.sh
